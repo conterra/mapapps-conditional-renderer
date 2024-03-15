bc2a04c792f3f85ce587553e849677037ba7388f
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2024-03-15 15:08:28 +0100

    Improve i18n

--- src/main/js/bundles/dn_conditionalrenderer/nls/bundle.js
+++ src/main/js/bundles/dn_conditionalrenderer/nls/bundle.js
@@ -17,9 +17,11 @@ export default {
     root: ({
         bundleName: "Conditional Renderer",
         bundleDescription: "This bundle makes it possible to create mutliple renderers and feature reductions for a layer to be used in different scale ranges, all by configuration.",
-        undefinedView: "conditionRenderer: No 'view' was found",
-        noRendererDefined: "conditionRenderer: No renderer defined",
-        rendererIncorrectDefined: "conditionRenderer: The renderer are wrongfully defined"
+        warnings: {
+            undefinedView: "conditionalRenderer: No 'view' was found",
+            noRendererDefined: "conditionalRenderer: No renderer defined",
+            rendererIncorrectDefined: "conditionalRenderer: The renderer are wrongfully defined"
+        }
     }),
     "de": true
 };
