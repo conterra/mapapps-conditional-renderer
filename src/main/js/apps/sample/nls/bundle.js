6bd1dc85e62a633c2b8758a436682b4fd0197b27
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-06-26 15:58:02 +0200

    Update sample app

--- src/main/js/apps/sample/nls/bundle.js
+++ src/main/js/apps/sample/nls/bundle.js
@@ -15,65 +15,20 @@
  */
 module.exports = {
     root: {
-        apptitle: "DevNet Bundle Blueprint",
+        apptitle: "Conditional Renderer Sample",
         map: {
-            koeln1: {
-                title: "Basic Data",
-                districts: {
-                    title: "City Districts",
-                    text: "Cologne's city district <b>{STV_NAME}</b>."
-                },
-                boroughs: {
-                    title: "Boroughs",
-                    text: "Boroughs <b>{NAME}</b> is located in Cologne's precints {STADTBEZIR}."
-                },
-                precints: {
-                    title: "Precints",
-                    text: "Cologne's precint {NAME}."
-                }
-            },
-            koeln2: {
-                title: "Education and Culture",
-                description: "List of all libraries, museums and schools in Cologne.",
-                libraries: {
-                    title: "Libraries"
-                },
-                museums: {
-                    title: "Museums",
-                    text: "Museum <b>{NAME}</b> is located in Cologne's boroughs {STADTTEIL}."
-                },
-                schools: {
-                    title: "Schools"
-                }
-            },
-            koeln3: {
-                title: "Recreation",
-                sights: {
-                    title: "Tourist Attractions",
-                    titleSingle: "Tourist Attraction",
-                    text: "Tourist attraction <b>{NAME}</b> is located in Cologne's borough {STADTTEIL}."
-                },
-                playgrounds: {
-                    title: "Playgrounds- and Sports Areas",
-                    text: "<b>{Spielplatzname}</b> is located in Cologne's borough {Stadtteil}.",
-                    baskets: "Basketball Baskets",
-                    goals: "Soccer Goals",
-                    tables: "Ping-Pong Tables",
-                    walls: "Goal Wall",
-                    skate: "Skating",
-                    misc: "Miscellaneous"
-                },
-                places: {
-                    title: "Places of Event",
-                    titleSingle: "Place of Event",
-                    text: "<b>{NAME}</b> is a {expression/carrier} place of event."
-                }
-            },
             basemaps: {
                 gray: "Street Map (gray)",
                 streets: "Street Map",
                 topo: "Topographical Map",
                 hybrid: "Aerial (hybrid)"
+            },
+            layers: {
+                treeLayerTitle: "Tree Cadastre",
+                clusterPopupTitle: "Cluster Summary",
+                clusterPopupContent: "This Cluster represents {cluster_count} trees.",
+                treeLayerPopupTitle: "Tree Info",
+                treeLayerPopup: "Tree height: {hoehe}m and trunk diameter: {stammdurchmesser}m. Tree was planted in {pflanzjahr}."
             }
         },
         tools: {
